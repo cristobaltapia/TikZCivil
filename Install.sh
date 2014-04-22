598d901fba6b7cec36c91b0333355367cc1daa11
Cristóbal Tapia <crtapia@gmail.com>
2014-04-22 14:09:58 -0300

    Installation file for linux was added.

---
+++ Install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp tikzcivil.sty ~/texmf/tex/latex/tikzcivil
+cp tikzcivil-structural.tex ~/texmf/tex/latex/tikzcivil
+cp tikzcivil-geomechanics.tex ~/texmf/tex/latex/tikzcivil
+
+texhash ~/texmf
