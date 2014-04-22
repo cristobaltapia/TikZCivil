f875ad35d832492506b9e03ec9ac2e0a7ada58f8
Cristóbal Tapia <crtapia@gmail.com>
2014-04-22 14:18:12 -0300

    Install file now check if the installation directory exists.

--- Install.sh
+++ Install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -p ~/texmf/tex/latex/tikzcivil
 cp tikzcivil.sty ~/texmf/tex/latex/tikzcivil
 cp tikzcivil-structural.tex ~/texmf/tex/latex/tikzcivil
 cp tikzcivil-geomechanics.tex ~/texmf/tex/latex/tikzcivil
